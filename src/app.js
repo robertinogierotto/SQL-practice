9af2c0dc9e3d4ebdf6a9612229c951f6c64fb3bb
robertinogierotto <robertino.gierotto@gmail.com>
2022-03-10 23:18:45 -0300

    CRUD on SQL-Sequalize

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ const indexRouter = require('./routes/index');
 
 const moviesRoutes = require('./routes/moviesRoutes');
 const genresRoutes = require('./routes/genresRoutes');
-const actorsRoutes = require('./routes/actorsRoutes')
+const actorsRoutes = require('./routes/actorsRoutes');
+
+
 const app = express();
 
 // view engine setup
 app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use('/', indexRouter);
--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,5 +1,4 @@
 const db = require ('../database/models');
-const sequelize = db.sequelize; //no se para que es
 
 const moviesController = {
     'list': async (req, res) => {
@@ -45,14 +44,77 @@ const moviesController = {
         db.Movie.findAll({
             where: {rating: {[db.Sequelize.Op.gte]: 8}},
             order: [
-                ['release_date', 'DESC']
+                ['rating', 'DESC']
               ]
          })
             .then(movies => {
                 res.render ('recommendedMovies', {movies})
             })
-    }
+    },
+    add: (req, res) => {
+        res.render('moviesAdd')
+    },
+    create: async function (req, res) {
+        try {
+            await db.Movie.create({
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length
+            })
+            res.redirect('/movies');
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    edit: async function (req, res) {
+        try {
+            const Movie = await db.Movie.findByPk(req.params.id);
+            res.render('moviesEdit', {Movie});
 
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    update: async function (req,res) {
+        try {
+            await db.Movie.update({
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length
+            },{
+                where: {id: req.params.id}
+            })
+            res.redirect('/movies');
+            
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    delete: async function (req, res) {
+        try {
+            const Movie = await db.Movie.findByPk(req.params.id);
+            res.render('moviesDelete', {Movie});
+            
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    destroy: async function (req, res) {
+        try {
+            await db.Movie.destroy({
+                where: {id: req.params.id}
+             });
+            res.redirect('/movies');
+             
+            
+        } catch (error) {
+            res.send(error)
+        }
+    }
 }
 
 module.exports = moviesController;
